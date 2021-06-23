ff3157038ea857eff46fdde0a65fddf1497359bb
hiroin <egamescape@gmail.com>
2021-06-23 15:14:24 +0200

    vectorTestMain.cppの713行目までOK

--- rainy/vectorTestMain.cpp
+++ rainy/vectorTestMain.cpp
@@ -380,17 +380,17 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "vector: size() of fill construction with 42, 42");
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.size() != ft_vec.size()) {
-  //     throw std::exception();
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error("failed");
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "vector: size() of fill construction with 42, 42");
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.size() != ft_vec.size()) {
+      throw std::exception();
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error("failed");
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no,
               "vector: max_size() of empty vector construct with default");
@@ -405,17 +405,17 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "vector: max_size() of fill construction with 42, 42");
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.max_size() != ft_vec.max_size()) {
-  //     throw std::exception();
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error("failed");
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "vector: max_size() of fill construction with 42, 42");
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.max_size() != ft_vec.max_size()) {
+      throw std::exception();
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error("failed");
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no,
               "vector: capacity() of empty vector construct with default");
@@ -430,18 +430,18 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "vector: capacity() of fill construction with 42, 42");
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.capacity() != ft_vec.capacity()) {
-  //     std::cout << std_vec.capacity() << std::endl;
-  //     throw std::exception();
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error("failed");
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "vector: capacity() of fill construction with 42, 42");
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.capacity() != ft_vec.capacity()) {
+      std::cout << std_vec.capacity() << std::endl;
+      throw std::exception();
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error("failed");
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no,
               "vector: empty() of empty vector construct with default");
@@ -456,130 +456,130 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "vector: empty() of fill construction with 42, 42");
-  // try {
-  //   std::vector<int> std_vec;
-  //   ft::vector<int> ft_vec;
-  //   if (std_vec.empty() != ft_vec.empty()) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error("failed");
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "vector: empty() of fill construction with 42, 42");
+  try {
+    std::vector<int> std_vec;
+    ft::vector<int> ft_vec;
+    if (std_vec.empty() != ft_vec.empty()) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error("failed");
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: vec[0] (int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   int std_res = std_vec[0];
-  //   int ft_res = ft_vec[0];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: vec[0] (int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    int std_res = std_vec[0];
+    int ft_res = ft_vec[0];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: vec[21] (int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   int std_res = std_vec[21];
-  //   int ft_res = ft_vec[21];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: vec[21] (int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    int std_res = std_vec[21];
+    int ft_res = ft_vec[21];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: vec[40] (int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   int std_res = std_vec[40];
-  //   int ft_res = ft_vec[40];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::out_of_range& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: vec[40] (int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    int std_res = std_vec[40];
+    int ft_res = ft_vec[40];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::out_of_range& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: assign vec[i] = i with constructed with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   for (int i = 0; i < 42; i++) {
-  //     std_vec[i] = i;
-  //     ft_vec[i] = i;
-  //     if (std_vec[i] != ft_vec[i]) {
-  //       throw std::runtime_error("failed");
-  //     }
-  //   }
-  // } catch (std::out_of_range& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: assign vec[i] = i with constructed with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    for (int i = 0; i < 42; i++) {
+      std_vec[i] = i;
+      ft_vec[i] = i;
+      if (std_vec[i] != ft_vec[i]) {
+        throw std::runtime_error("failed");
+      }
+    }
+  } catch (std::out_of_range& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: vec[0] (const int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   const int std_res = std_vec[0];
-  //   const int ft_res = ft_vec[0];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: vec[0] (const int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    const int std_res = std_vec[0];
+    const int ft_res = ft_vec[0];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: vec[21] (const int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   const int std_res = std_vec[21];
-  //   const int ft_res = ft_vec[21];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: vec[21] (const int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    const int std_res = std_vec[21];
+    const int ft_res = ft_vec[21];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no,
-  //             "Vector: vec[40] (const int) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   const int std_res = std_vec[40];
-  //   const int ft_res = ft_vec[40];
-  //   if (std_res != ft_res) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no,
+              "Vector: vec[40] (const int) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    const int std_res = std_vec[40];
+    const int ft_res = ft_vec[40];
+    if (std_res != ft_res) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no, "Vector: at(0) of default constructed vector");
   flg = 0;
@@ -596,45 +596,45 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: at(0) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.at(0) != ft_vec.at(0)) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: at(0) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.at(0) != ft_vec.at(0)) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: at(21) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.at(21) != ft_vec.at(21)) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: at(21) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.at(21) != ft_vec.at(21)) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: at(41) of fill construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   if (std_vec.at(41) != ft_vec.at(41)) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::out_of_range& e) {
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: at(41) of fill construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    if (std_vec.at(41) != ft_vec.at(41)) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::out_of_range& e) {
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no, "Vector: reserve(42) for default construction");
   flg = 0;
@@ -661,54 +661,54 @@ int main()
   }
   std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: reserve(42) for construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   std_vec.reserve(42);
-  //   ft_vec.reserve(42);
-  //   std_vec[21] = 21;
-  //   ft_vec[21] = 21;
-  //   std_vec[41] = 41;
-  //   ft_vec[41] = 41;
-  //   if (std_vec.capacity() != ft_vec.capacity() ||
-  //       std_vec.size() != ft_vec.size() ||
-  //       std_vec.max_size() != ft_vec.max_size() || std_vec[41] != ft_vec[41] ||
-  //       std_vec[21] != ft_vec[21]) {
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: reserve(42) for construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    std_vec.reserve(42);
+    ft_vec.reserve(42);
+    std_vec[21] = 21;
+    ft_vec[21] = 21;
+    std_vec[41] = 41;
+    ft_vec[41] = 41;
+    if (std_vec.capacity() != ft_vec.capacity() ||
+        std_vec.size() != ft_vec.size() ||
+        std_vec.max_size() != ft_vec.max_size() || std_vec[41] != ft_vec[41] ||
+        std_vec[21] != ft_vec[21]) {
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
-  // putTestInfo(test_no, "Vector: reserve(84) for construction with 42, 42");
-  // flg = 0;
-  // try {
-  //   std::vector<int> std_vec(42, 42);
-  //   ft::vector<int> ft_vec(42, 42);
-  //   std_vec.reserve(84);
-  //   ft_vec.reserve(84);
-  //   std_vec[21] = 21;
-  //   ft_vec[21] = 21;
-  //   std_vec[41] = 41;
-  //   ft_vec[41] = 41;
-  //   std_vec[83] = 83;
-  //   ft_vec[83] = 83;
-  //   if (std_vec.capacity() != ft_vec.capacity() ||
-  //       std_vec.size() != ft_vec.size() ||
-  //       std_vec.max_size() != ft_vec.max_size() || std_vec[41] != ft_vec[41] ||
-  //       std_vec[21] != ft_vec[21] || std_vec[83] != ft_vec[83]) {
-  //     // std::cout << "24: " << std_vec[24] << ", " << ft_vec[24] << std::endl;
-  //     // std::cout << "41: " << std_vec[41] << ", " << ft_vec[41] << std::endl;
-  //     // std::cout << "80: " << std_vec[83] << ", " << ft_vec[83] << std::endl;
-  //     throw std::runtime_error("failed");
-  //   }
-  // } catch (std::exception& e) {
-  //   throw std::runtime_error(e.what());
-  // }
-  // std::cout << " => OK :)" << std::endl;
+  putTestInfo(test_no, "Vector: reserve(84) for construction with 42, 42");
+  flg = 0;
+  try {
+    std::vector<int> std_vec(42, 42);
+    ft::vector<int> ft_vec(42, 42);
+    std_vec.reserve(84);
+    ft_vec.reserve(84);
+    std_vec[21] = 21;
+    ft_vec[21] = 21;
+    std_vec[41] = 41;
+    ft_vec[41] = 41;
+    std_vec[83] = 83;
+    ft_vec[83] = 83;
+    if (std_vec.capacity() != ft_vec.capacity() ||
+        std_vec.size() != ft_vec.size() ||
+        std_vec.max_size() != ft_vec.max_size() || std_vec[41] != ft_vec[41] ||
+        std_vec[21] != ft_vec[21] || std_vec[83] != ft_vec[83]) {
+      // std::cout << "24: " << std_vec[24] << ", " << ft_vec[24] << std::endl;
+      // std::cout << "41: " << std_vec[41] << ", " << ft_vec[41] << std::endl;
+      // std::cout << "80: " << std_vec[83] << ", " << ft_vec[83] << std::endl;
+      throw std::runtime_error("failed");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
 
   putTestInfo(test_no, "Vector: reserve(INT_MAX) for default construction");
   flg = 0;
