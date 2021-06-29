3c6619f4332186e936fcf0f61de14f178c8cb3b9
hiroin <egamescape@gmail.com>
2021-06-29 02:41:22 +0200

    vector<bool>作成準備完了

--- rainy/vector.hpp
+++ rainy/vector.hpp
@@ -731,6 +731,719 @@ bool lexicographical_compare(
   return first1 == last1 && first2 != last2;
 }
 
+
+
+
+
+
+template < typename Allocator >
+class vector<bool, Allocator>
+
+// template < class Allocator >
+// class vector<bool, Allocator>
+{
+ public :
+  typedef bool value_type;
+  typedef bool* pointer;
+  typedef const pointer const_pointer;
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef Allocator allocator_type;
+  typedef std::size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+  // typedef pointer iterator;
+  typedef vectorIterator<bool> iterator;
+  // typedef const_pointer const_iterator;
+  typedef vectorConstIterator<bool> const_iterator;
+  // typedef std::reverse_iterator<pointer> reverse_iterator;
+  typedef reverseIterator<iterator> reverse_iterator;
+  // typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+  typedef reverseIterator<const_iterator> const_reverse_iterator;
+
+private :
+  // 先頭の要素へのポインター
+  pointer first_;
+  // 最後の要素の1つ前方のポインター
+  pointer last_;
+  // 確保したストレージの終端
+  pointer reserved_last_;
+  // アロケーターの値
+  allocator_type alloc_;
+
+public :
+  // コンストラクター
+  vector(const allocator_type & alloc_ = allocator_type())
+    : alloc_(alloc_), first_(NULL), last_(NULL), reserved_last_(NULL)
+  {};
+  // vector( size_type size, const allocator_type & alloc_ = allocator_type() )
+  //   : alloc_(alloc_), first_(NULL), last_(NULL), reserved_last_(NULL)
+  // {
+  //   resize(size);
+  // }
+  vector( size_type size, const_reference value = value_type(), const allocator_type & alloc_ = allocator_type() )
+    : alloc_(alloc_), first_(NULL), last_(NULL), reserved_last_(NULL)
+  {
+    resize(size, value);
+  }
+  template <typename InputIterator>
+  vector(InputIterator first,
+    typename ft_enable_if<!ft_is_integral<InputIterator>::value, InputIterator>::type last, const Allocator & = Allocator())
+    : first_(NULL), last_(NULL), reserved_last_(NULL)
+  {
+    reserve(getSizeFromIterator(first, last));
+    for (InputIterator i = first; i != last ; i++)
+    {
+      push_back(*i);
+    }
+  }
+
+  // デストラクター
+  ~vector()
+  {
+    // 1. 要素を末尾から先頭に向かう順番で破棄
+    clear();
+    // 2. 生のメモリーを解放する
+    deallocate();
+  };
+
+  // コピー
+  vector(const vector & r)
+    : alloc_(r.alloc_), first_(NULL), last_(NULL), reserved_last_(NULL)
+  {
+    // コピー元の要素数を保持できるだけのストレージを確保
+    reserve(r.size());
+    // コピー元の要素をコピー構築
+    // destはコピー先
+    // [src, last_)はコピー元
+    pointer dest = first_;
+    for (const_iterator src = r.begin(), last_ = r.end();
+      src != last_ ; ++dest, ++src)
+    {
+      construct(dest, *src);
+    }
+    last_ = first_ + r.size();
+  }
+
+  vector & operator =(const vector & r)
+  {
+    // 1. 自分自身への代入なら何もしない
+    if (this == &r)
+      return *this;
+  
+    // 2. 要素数が同じならば
+    if (size() == r.size())
+    {   // 要素ごとにコピー代入
+      // std::copy(r.begin(), r.end(), begin()) ;
+      iterator dest_iter = begin();
+      for (const_iterator src_iter = r.begin(), src_end = r.end();
+        src_iter != src_end; ++src_iter, ++dest_iter)
+      {
+        *dest_iter = *src_iter;
+      }      
+    }
+    // 3. それ以外の場合で
+    else 
+      // 予約数が十分ならば、
+      if (capacity() >= r.size())
+      {
+        iterator dest_iter;
+        iterator dest_end = end();
+        const_iterator src_iter;
+        const_iterator src_end = r.end();
+        for (dest_iter = begin() + r.size(), dest_end = end();
+          dest_iter != dest_end;
+          ++dest_iter)
+        {
+          destroy(&*dest_iter);
+        }
+        if (size() >= r.size())
+        {
+          last_ = first_;
+          for (src_iter = r.begin(), dest_iter = begin();
+            src_iter != src_end;
+            ++src_iter, ++dest_iter, last_++)
+          {
+            *dest_iter = *src_iter;
+          }
+        }
+        else
+        {
+          last_ = first_;
+          for (src_iter = r.begin(), dest_iter = begin();
+            dest_iter != dest_end;
+            ++src_iter, ++dest_iter, last_++)
+          {
+            *dest_iter = *src_iter;
+          }
+          for (src_end = r.end();
+            src_iter != src_end;
+            ++src_iter, ++dest_iter, ++last_)
+          {
+            construct(&*dest_iter, *src_iter);
+          }
+        }
+        // clear();
+        // iterator dest_iter = begin();
+        // for (const_iterator src_iter = r.begin(), src_end = r.end();
+        //   src_iter != src_end; ++src_iter, ++dest_iter, ++last_)
+        // {
+        //   construct(&*dest_iter, *src_iter);
+        // }
+      }
+      // 4. 予約数が不十分ならば
+      else
+      {
+        // 要素をすべて破棄
+        // destroy_all();
+        clear();
+        // 予約
+        reserve(r.size());
+        // コピー構築
+        iterator dest_iter = begin();
+        for (const_iterator src_iter = r.begin(), src_end = r.end();
+          src_iter != src_end; ++src_iter, ++dest_iter, ++last_)
+        {
+          construct(&*dest_iter, *src_iter);
+        }
+      }
+    return *this ;
+  }
+  bool equal(iterator first, iterator last, const_iterator rfirst)
+  {
+    const_iterator cfirst = first;
+    const_iterator clast = last;
+    for (; cfirst != clast; ++cfirst, ++rfirst)
+    {
+      if (*cfirst != *rfirst)
+        return false;
+    }
+    return true;
+  }
+  bool operator==(const vector<bool, Allocator>& r)
+  {
+    return (this->size() == r.size()
+      && equal(this->begin(), this->end(), r.begin()));
+  }
+  bool operator!=(const vector<bool, Allocator>& r)
+  {
+    return !(*this == r);
+  }
+  bool operator<(const vector<bool, Allocator>& r)
+  {
+    return lexicographical_compare(begin(), end(), r.begin(), r.end());
+  }
+  bool operator<=(const vector<bool, Allocator>& r)
+  {
+    return !(*this > r);
+  }
+  bool operator>(const vector<bool, Allocator>& r)
+  {
+    return lexicographical_compare(r.begin(), r.end(), begin(), end());
+  }
+  bool operator>=(const vector<bool, Allocator>& r)
+  {
+    return !(*this < r);
+  }
+
+  // イテレーターアクセス
+  iterator begin()
+  {
+    return iterator(first_);
+  }
+  iterator end()
+  {
+    return iterator(last_);
+  }
+  const_iterator begin() const
+  {
+    return const_iterator(first_);
+  }
+  const_iterator end() const
+  {
+    return const_iterator(last_);
+  }
+  reverse_iterator rbegin()
+  {
+    return reverse_iterator(end());
+  }
+  reverse_iterator rend()
+  {
+    return reverse_iterator(begin());
+  }
+  const_reverse_iterator rbegin() const
+  {
+    return const_reverse_iterator(end());
+  }
+   const_reverse_iterator rend() const
+  {
+    return const_reverse_iterator(begin());
+  }
+
+  size_type size() const
+  {
+    return end() - begin();
+  }
+  size_type max_size() const
+  {
+    return (alloc_.max_size() / 2);
+  }
+  bool empty() const
+  {
+    // return size() == 0;
+    return begin() == end();
+  }
+  size_type capacity() const
+  {
+    return reserved_last_ - first_;
+  }  
+
+  // 要素アクセス
+  reference operator [](size_type i)
+  {
+    return first_[i];
+  }
+  const_reference operator [](size_type i) const
+  {
+    return first_[i];
+  }
+  reference at( size_type i )
+  {
+    if ( i >= size() )
+      throw std::out_of_range( "vector" ) ;
+    return first_[i] ;
+  }
+  const_reference at( size_type i ) const
+  {
+    if ( i >= size() )
+      throw std::out_of_range( "vector" ) ;
+    return first_[i] ;
+  }
+  reference front()
+  {
+    return *begin();
+  }
+  const_reference front() const
+  {
+    return *begin();
+  }
+  reference back()
+  {
+    iterator tmp = end();
+    --tmp;
+    return *tmp;
+    // return last_ - 1;
+
+  }
+  const_reference back() const
+  {
+    const_iterator tmp = end();
+    --tmp;
+    return *tmp;
+    // return last_ - 1;
+  }
+
+  void reserve(size_type sz)
+  {
+    // すでに指定された要素数以上に予約されているなら何もしない
+    if (sz <= capacity())
+      return ;
+
+    // 動的メモリー確保をする
+    pointer ptr = allocate(sz) ;
+
+    // 古いストレージの情報を保存
+    pointer old_first_ = first_;
+    pointer old_last_ = last_;
+    size_type old_capacity = capacity();
+
+    // 新しいストレージに差し替え
+    first_ = ptr;
+    last_ = first_;
+    reserved_last_ = first_ + sz;
+
+    // 例外安全のため
+    // 関数を抜けるときに古いストレージを破棄する
+    // C++20の機能
+    // std::scope_exit e( [&]{
+    //   traits::deallocate(alloc_, old_first_, old_capacity);
+    // } );
+
+    // 古いストレージから新しいストレージに要素をコピー構築
+    for (iterator old_iter = old_first_, old_end_ = old_last_;
+      old_iter != old_end_; ++old_iter, ++last_)
+    {
+      // construct(last_, std::move(*old_iter)) ;
+      construct(last_, *old_iter) ;
+    }
+
+    // 新しいストレージにコピーし終えたので
+    // 古いストレージの値は破棄
+    for (reverse_iterator riter = reverse_iterator(old_last_), rend = reverse_iterator(old_first_);
+          riter != rend ; ++riter)
+    {
+      destroy(&*riter);
+    }
+    // ストレージを破棄する
+    alloc_.deallocate(old_first_, old_capacity);
+  }
+
+  // void resize(size_type sz)
+  // {
+  //   // 現在の要素数より少ない
+  //   if (sz < size())
+  //   {
+  //     size_type diff = size() - sz;
+  //     destroy_until(rbegin() + diff) ;
+  //     last_ = first_ + sz ;
+  //   }
+  //   // 現在の要素数より大きい
+  //   else if ( sz > size() )
+  //   {
+  //     reserve(sz) ;
+  //     for (; last_ != reserved_last_ ; ++last_)
+  //     {
+  //       construct(last_);
+  //     }
+  //   }
+  // }
+
+  void resize(size_type sz, const_reference value = value_type())
+  {
+    // 現在の要素数より少ない
+    if (sz < size())
+    {
+      size_type diff = size() - sz;
+      destroy_until(rbegin() + diff) ;
+      last_ = first_ + sz ;
+    }
+    // 現在の要素数より大きい
+    else if ( sz > size() )
+    {
+      // insertの処理と一緒 ( https://cpprefjp.github.io/reference/vector/vector/resize.html )
+      if (sz < size() * 2)
+        reserve(size() * 2);
+      else
+        reserve(sz);
+      for (size_t i = 0; sz - size(); ++i, ++last_)
+      {
+        construct(last_, value);
+      }
+      // for (; last_ != reserved_last_ ; ++last_)
+      // {
+      //   construct(last_, value);
+      // }
+    }
+  }
+
+  void push_back(const_reference value) 
+  {
+    // 予約メモリーが足りなければ拡張
+    if (size() + 1 > capacity())
+    {
+        // 現在のストレージサイズ
+        size_type c = size();
+        // 0の場合は1に
+        if (c == 0)
+            c = 1 ;
+        else
+            // それ以外の場合は2倍する
+            c *= 2 ;
+        reserve(c) ;
+    }
+    construct(last_, value);
+    ++last_;
+  }
+
+  template <class InputIterator>
+  void assign(InputIterator first,
+    typename ft_enable_if<!ft_is_integral<InputIterator>::value, InputIterator>::type last)
+  {
+    vector<bool> tmp(first, last);
+    *this = tmp;
+  }
+
+  void assign(size_type n, const bool& u)
+  {
+    vector<bool> tmp(n, u);
+    *this = tmp;
+  }
+
+  void pop_back()
+  {
+    if (empty() == false)
+    {
+      last_--;
+      destroy(last_);
+    }
+  }
+
+  iterator insert(iterator position, const bool& x)
+  {
+    if (position == iterator(NULL))
+    {
+      assign(1, x);
+      return begin();
+    }
+    else if
+    (position == end())
+    {
+      push_back(x);
+      return end() - 1;
+    }
+    const size_type index = position - begin();
+    const size_type numOfMove = end() - position;
+    if (size() + 1 > capacity())
+    {
+      size_type c = size();
+      if (c == 0)
+          c = 1 ;
+      else
+          c *= 2 ;
+      reserve(c) ;
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        // *(end() - i) = *(end() - i - 1);
+        construct((end() - i).ptr_, *(end() - i - 1));
+        destroy((end() - i - 1).ptr_);
+      }
+      // destroy(&first_[index]);
+      construct(&first_[index], x);
+    }
+    else
+    {
+      construct(&*end(), *(end() - 1));
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        *(end() - i) = *(end() - i - 1);
+      }
+      first_[index] = x;
+    }
+    last_++;
+    return iterator(&first_[index]);
+  }
+
+  void insert(iterator position, size_type n, const bool& x)
+  {
+    if (position == iterator(NULL))
+    {
+      assign(n, x);
+      return;
+    }
+    size_type index = position - begin();
+    size_type numOfMove = end() - position;
+    if (size() + n > capacity())
+    {
+      size_type c = size();
+      if (c == 0)
+        c = n ;
+      else if(size() + n < c * 2)
+        c *= 2;
+      else
+        c = size() + n;
+      reserve(c) ;
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        // *(end() + n - 1 - i) = *(end() - 1 - i);
+        construct(&*(end() + n - 1 - i), *(end() - i - 1));
+        destroy(&*(end() - i - 1));        
+      }
+      for (size_t i = 0; i < n; ++i)
+      {
+        construct(&first_[index++], x);
+        // first_[index++] = x;
+        last_++;
+      }
+    }
+    else
+    {
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        if ((end() + n - 1 - i) >= end())
+          construct(&*(end() + n - 1 - i), *(end() - 1 - i));
+        else
+          *(end() + n - 1 - i) = *(end() - 1 - i);
+      }
+      iterator oldEnd = end();
+      for (size_t i = 0; i < n; ++i)
+      {
+        if (begin() + index >= oldEnd)
+          construct(&first_[index++], x);
+        else
+          first_[index++] = x;
+        last_++;
+      }
+    }
+  }
+
+  template <class InputIterator>
+  void insert(iterator position,
+    typename ft_enable_if<!ft_is_integral<InputIterator>::value, InputIterator>::type first, InputIterator last)
+  {
+    if (position == iterator(NULL))
+    {
+      assign(first, last);
+      return;
+    }
+    size_type index = position - begin();
+    size_type numOfMove = end() - position;
+    size_type n = getSizeFromIterator(first, last);
+    if (size() + n > capacity())
+    {
+      size_type c = size();
+      if (c == 0)
+        c = n ;
+      else if(size() + n < c * 2)
+        c *= 2;
+      else
+        c = size() + n;
+      reserve(c) ;
+
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        // *(end() - i) = *(end() - i - 1);
+        construct(&*(end() + n - 1 - i), *(end() - i - 1));
+        destroy(&*(end() - i - 1));
+      }
+      for (size_t i = 0; i < n; ++i)
+      {
+        construct(&first_[index++], *first++);
+        // first_[index++] = *first;
+        last_++;
+      }      
+    }
+    else
+    {
+      for (size_t i = 0; i < numOfMove; ++i)
+      {
+        *(end() + n - 1 - i) = *(end() - 1 - i);
+      }
+      for (size_t i = 0; i < n; ++i)
+      {
+        construct(&first_[index++], *first++);
+        // first_[index++] = *first;
+        // first++;
+        last_++;
+      }
+    }
+  }
+
+  iterator erase(iterator position)
+  {
+    for (iterator itr = position; itr != end() - 1; ++itr)
+    {
+      *itr = *(itr + 1);
+    }
+    last_--;
+    destroy(last_);
+    return position;
+  }
+
+  iterator erase(iterator first, iterator last)
+  {
+    iterator firstPosition = first;
+    size_type n = end() - last;
+    for (size_t i = 0; i < n; ++i)
+    {
+      *first++ = *last++;
+    }
+    // last_ -= last - first;
+    for (size_t i = 0; i < getSizeFromIterator(first, last); ++i)
+    {
+      last_--;
+      destroy(last_);
+    }
+    return firstPosition;
+  }
+
+  void swap(vector& x)
+  {
+    pointer first = x.first_;
+    pointer last = x.last_;
+    pointer reserved_last = x.reserved_last_;
+
+    x.first_ = this->first_;
+    x.last_ = this->last_;
+    x.reserved_last_ = this->reserved_last_;
+    this->first_ = first;
+    this->last_ = last;
+    this->reserved_last_ = reserved_last;
+  }
+  allocator_type get_allocator() const
+  {
+    return alloc_;
+  }
+  void clear()
+  {
+    destroy_until(rend());
+  }
+
+ private :
+  pointer allocate(size_type n)
+  {
+    // return traits::allocate(alloc_, n);
+    return alloc_.allocate(n);
+  }
+  void deallocate()
+  {
+    if (capacity() > 0)
+      // traits::deallocate(alloc_, first_, capacity());
+      alloc_.deallocate(first_, capacity());
+  }
+  // この関数いるのかな…
+  void construct(pointer ptr)
+  {
+    // traits::construct(alloc_, ptr);
+    alloc_.construct(ptr, value_type());
+  }
+  void construct(pointer ptr, const_reference value)
+  {
+    // traits::construct(alloc_, ptr, value);
+    alloc_.construct(ptr, value);
+  }
+  // ムーブ用
+  // c++11の機能
+  // void construct(pointer ptr, value_type && value)
+  // {
+  //   traits::construct(alloc_, ptr, std::move(value));
+  // }
+  void destroy(pointer ptr)
+  {
+    // traits::destroy(alloc_, ptr);
+    alloc_.destroy(ptr);
+   }
+  void destroy_until(reverse_iterator rend)
+  {
+    for (reverse_iterator riter = rbegin(); riter != rend; ++riter, --last_)
+    {
+      // 簡易vector<T>のiteratorは単なるT *だが、
+      // riterはリバースイテレーターなのでポインターではない。
+      // ポインターを取るために*riterでまずT &を得て、
+      // そこに&を適用することでT *を得ている。
+      destroy( &*riter );
+    }
+  }
+
+  template <class InputIterator>
+  size_t getSizeFromIterator(InputIterator first, InputIterator last)
+  {
+    InputIterator iter = first;
+    size_type n = 0;
+    while (iter != last)
+    {
+      ++iter;
+      ++n;
+    }
+    return n;
+  }
+};
+
+
+
+
+
+
+
+
 }; // namespace ft
 
 #endif /* VECTOR_HPP */
---
+++ rainy/vectorBoolTestMain.cpp
@@ -0,0 +1,234 @@
+#include <exception>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <vector>
+
+#include "test.hpp"
+#include "vector.hpp"
+
+static void test_value_assign(int& test_no, size_t size1, bool val1,
+                              size_t size2, bool val2) {
+  std::stringstream sout;
+  sout << std::boolalpha << "Vector<bool>: vector(" << size1 << ", " << val1
+       << ") and assign(" << size2 << ", " << val2 << ")";
+  putTestInfo(test_no, sout.str());
+  try {
+    std::vector<bool> std_vec(size1, val1);
+    ft::vector<bool> ft_vec(size1, val1);
+
+    std_vec.assign(size2, val2);
+    ft_vec.assign(size2, val2);
+    for (size_t idx = 0; idx < size2; ++idx) {
+      if (std_vec[idx] != ft_vec[idx]) {
+        std::cout << std::endl << "idx = " << idx << std::endl;
+        std::cout << "std: " << std_vec[idx] << std::endl;
+        std::cout << " ft: " << ft_vec[idx] << std::endl;
+        throw std::runtime_error("value");
+      }
+    }
+    if (std_vec.empty() != ft_vec.empty()) {
+      throw std::runtime_error("empty");
+    }
+    if (std_vec.size() != ft_vec.size()) {
+      throw std::runtime_error("size");
+    }
+    if (std_vec.capacity() != ft_vec.capacity()) {
+      std::cout << std::endl;
+      std::cout << "std: " << std_vec.capacity() << std::endl;
+      std::cout << " ft: " << ft_vec.capacity() << std::endl;
+      throw std::runtime_error("value");
+      throw std::runtime_error("capacity");
+    }
+    if (std_vec.max_size() != ft_vec.max_size()) {
+      throw std::runtime_error("max_size");
+    }
+  } catch (std::runtime_error& e) {
+    throw e;
+  }
+  std::cout << " => OK :)" << std::endl;
+}
+
+void test_vector_bool(int& test_no) {
+  std::cout << "=== TEST VECTOR<BOOL> ===" << std::endl;
+  std::cout << std::boolalpha;
+  size_t size = 0;
+  size_t size_ary[] = {0,   1,   2,    63,   64,   65,   100, 127,
+                       128, 129, 1023, 1024, 1025, 2000, 4000};
+  bool flg = 0;
+
+  putTestInfo(test_no, "Vector<bool>: size/capacity of default construction");
+  try {
+    std::vector<bool> std_vec;
+    ft::vector<bool> ft_vec;
+
+    if (std_vec.empty() != ft_vec.empty()) {
+      throw std::runtime_error("empty");
+    }
+    if (std_vec.size() != ft_vec.size()) {
+      throw std::runtime_error("size");
+    }
+    if (std_vec.capacity() != ft_vec.capacity()) {
+      throw std::runtime_error("capacity");
+    }
+    if (std_vec.max_size() != ft_vec.max_size()) {
+      std::cout << std::endl;
+      std::cout << "max_size(std) = " << std_vec.max_size() << std::endl;
+      std::cout << "max_size(ft)  = " << ft_vec.max_size() << std::endl;
+      throw std::runtime_error("max_size");
+    }
+  } catch (std::runtime_error& e) {
+    throw e;
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  for (size = 0; size <= 150; size++) {
+    std::stringstream sout;
+    sout << "Vector<bool>: vector(" << size << ")";
+    putTestInfo(test_no, sout.str());
+    try {
+      std::vector<bool> std_vec(size);
+      ft::vector<bool> ft_vec(size);
+
+      for (size_t idx = 0; idx < size; ++idx) {
+        if (std_vec[idx] != ft_vec[idx]) {
+          std::cout << std::endl << "idx = " << idx << std::endl;
+          std::cout << "std: " << std_vec[idx] << std::endl;
+          std::cout << " ft: " << ft_vec[idx] << std::endl;
+          throw std::runtime_error("value");
+        }
+      }
+      if (std_vec.empty() != ft_vec.empty()) {
+        throw std::runtime_error("empty");
+      }
+      if (std_vec.size() != ft_vec.size()) {
+        throw std::runtime_error("size");
+      }
+      if (std_vec.capacity() != ft_vec.capacity()) {
+        throw std::runtime_error("capacity");
+      }
+      if (std_vec.max_size() != ft_vec.max_size()) {
+        throw std::runtime_error("max_size");
+      }
+    } catch (std::runtime_error& e) {
+      throw e;
+    }
+    std::cout << " => OK :)" << std::endl;
+  }
+
+  for (size = 0; size <= 150; size++) {
+    std::stringstream sout;
+    sout << "Vector<bool>: vector(" << size << ", false)";
+    putTestInfo(test_no, sout.str());
+    try {
+      std::vector<bool> std_vec(size, false);
+      ft::vector<bool> ft_vec(size, false);
+
+      for (size_t idx = 0; idx < size; ++idx) {
+        if (std_vec[idx] != ft_vec[idx]) {
+          std::cout << std::endl << "idx = " << idx << std::endl;
+          std::cout << "std: " << std_vec[idx] << std::endl;
+          std::cout << " ft: " << ft_vec[idx] << std::endl;
+          throw std::runtime_error("value");
+        }
+      }
+      if (std_vec.empty() != ft_vec.empty()) {
+        throw std::runtime_error("empty");
+      }
+      if (std_vec.size() != ft_vec.size()) {
+        throw std::runtime_error("size");
+      }
+      if (std_vec.capacity() != ft_vec.capacity()) {
+        throw std::runtime_error("capacity");
+      }
+      if (std_vec.max_size() != ft_vec.max_size()) {
+        throw std::runtime_error("max_size");
+      }
+    } catch (std::runtime_error& e) {
+      throw e;
+    }
+    std::cout << " => OK :)" << std::endl;
+  }
+
+  for (size = 0; size <= 150; size++) {
+    std::stringstream sout;
+    sout << "Vector<bool>: vector(" << size << ", true)";
+    putTestInfo(test_no, sout.str());
+    try {
+      std::vector<bool> std_vec(size, false);
+      ft::vector<bool> ft_vec(size, false);
+
+      for (size_t idx = 0; idx < size; ++idx) {
+        if (std_vec[idx] != ft_vec[idx]) {
+          std::cout << std::endl << "idx = " << idx << std::endl;
+          std::cout << "std: " << std_vec[idx] << std::endl;
+          std::cout << " ft: " << ft_vec[idx] << std::endl;
+          throw std::runtime_error("value");
+        }
+      }
+      if (std_vec.empty() != ft_vec.empty()) {
+        throw std::runtime_error("empty");
+      }
+      if (std_vec.size() != ft_vec.size()) {
+        throw std::runtime_error("size");
+      }
+      if (std_vec.capacity() != ft_vec.capacity()) {
+        throw std::runtime_error("capacity");
+      }
+      if (std_vec.max_size() != ft_vec.max_size()) {
+        throw std::runtime_error("max_size");
+      }
+    } catch (std::runtime_error& e) {
+      throw e;
+    }
+    std::cout << " => OK :)" << std::endl;
+  }
+
+  size = 420;
+  putTestInfo(test_no, "Vector<bool>: assign random to vec(420)");
+  try {
+    bool val;
+    std::vector<bool> std_vec(size);
+    ft::vector<bool> ft_vec(size);
+
+    for (size_t idx = 0; idx < size; ++idx) {
+      val = rand() % 2;
+      std_vec[idx] = val;
+      ft_vec[idx] = val;
+    }
+    for (size_t idx = 0; idx < size; ++idx) {
+      if (std_vec[idx] != ft_vec[idx]) {
+        std::cout << std::endl << "idx = " << idx << std::endl;
+        std::cout << "std: " << std_vec[idx] << std::endl;
+        std::cout << " ft: " << ft_vec[idx] << std::endl;
+        throw std::runtime_error("value");
+      }
+    }
+    if (std_vec.empty() != ft_vec.empty()) {
+      throw std::runtime_error("empty");
+    }
+    if (std_vec.size() != ft_vec.size()) {
+      throw std::runtime_error("size");
+    }
+    if (std_vec.capacity() != ft_vec.capacity()) {
+      throw std::runtime_error("capacity");
+    }
+    if (std_vec.max_size() != ft_vec.max_size()) {
+      std::cout << std::endl;
+      std::cout << "max_size(std) = " << std_vec.max_size() << std::endl;
+      std::cout << "max_size(ft)  = " << ft_vec.max_size() << std::endl;
+      throw std::runtime_error("max_size");
+    }
+  } catch (std::runtime_error& e) {
+    throw e;
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  std::cout << std::noboolalpha;
+}
+
+int main()
+{
+  int no = 1;
+  test_vector_bool(no);
+}
